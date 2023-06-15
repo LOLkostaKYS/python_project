d641cc436a618dac0fc39fd4bef67d6b814b71a9
LOLkostaKYS <konstantinoschantz07@gmail.com>
2023-06-15 16:03:53 +0300

    first

---
+++ ASK (1).py
@@ -0,0 +1,4 @@
+height = input("Height(cm): ")
+lenght = input("Length(cm): ")
+perimeter = int(height) * int(lenght)
+print ("The perimeter of the rectangle is:", perimeter, "cm")
---
+++ ASK (2).py
@@ -0,0 +1,6 @@
+small_base = input("Small base(cm): ")
+big_base = input("Big base(cm): ") 
+height1 = input("Height1(cm): ")
+height2 = input("Height2(cm): ")
+volume = (int(small_base) * int(big_base)) / 2
+print(volume)
